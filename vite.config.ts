eba056f8a3f017d27c7115e326c40f0a9281dc0a
xiling_hushaoyu <husy@sit.com.cn>
2026-01-29 17:30:59 +0800

    Update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   // 添加 base 配置，根据实际部署路径调整
-  base: './',
+  base: '/bookMark/',
   plugins: [
     react(),
     VitePWA({
